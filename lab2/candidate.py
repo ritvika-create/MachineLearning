824608110c9b70cd46642fb7d66f3238d7066318
ritvika-create <71486418+ritvika-create@users.noreply.github.com>
2022-06-17 09:43:49 +0530

    Create candidate.py

---
+++ lab2/candidate.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+dataset= pd.read_csv('/079/lab3/tennis.csv',names=['outlook','temperature','humidity','wind','class',])
+
+def entropy(target_col):
+  elements,counts = np.unique(target_col,return_counts = True)
+  entropy   =   np.sum([(-counts[i]/np.sum(counts))*np.log2(counts[i]/np.sum(counts))
+  import numpy as np  
+import pandas as pd
+
+data = pd.DataFrame(data=pd.read_csv('enjoysport.csv'))  
+concepts = np.array(data.iloc[:,0:-1]) 
+print('Concepts:', concepts)  
+target = np.array(data.iloc[:,-1])   
+print('Target:', target) 
+ 
+def learn(concepts, target):  
+    print("Initialization of specific_h and general_h")      
+    
+    specific_h = concepts[0].copy()      
+    print('\t specific_h:', specific_h)
+
+    general_h = [["?" for i in range(len(specific_h))] for i in range(len(specific_h))]      
+    print('\t general_h:', general_h)
+
+    for i, h in enumerate(concepts):  
+        if target[i] == "yes":  
+            for x in range(len(specific_h)):  
+                if h[x]!= specific_h[x]:                     
+                    specific_h[x] ='?'                      
+                    general_h[x][x] ='?'  
+        if target[i] == "no":             
+            for x in range(len(specific_h)):  
+                if h[x]!= specific_h[x]:                     
+                    general_h[x][x] = specific_h[x]                 
+                else:                     
+                    general_h[x][x] = '?'         
+        
+        print("\n Steps of Candidate Elimination Algorithm",i+1)
+        print('\t specific_h', specific_h)       
+        print('\t general_h:', general_h)
+
+    indices = [i for i, val in enumerate(general_h) if val == ['?', '?', '?', '?', '?', '?']]     
+    for i in indices:    
+        general_h.remove(['?', '?', '?', '?', '?', '?'])  
+    return specific_h, general_h
+
+s_final, g_final = learn(concepts, target)
+
+print("\n Final specific_h:", s_final, sep="\n")
+print("\n Final general_h:", g_final, sep="\n")
